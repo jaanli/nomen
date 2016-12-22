57ea3b3e4738b8a996cb05d3fd1ed41f25322034
Jaan Altosaar <jaan.altosaar@gmail.com>
2016-12-22 10:56:28 -0500

    add version, upload to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,118 @@
-from setuptools import setup
+#!/usr/bin/env python
+# setup
+# Setup script for installing nomen
 
-setup(name='nomen',
-      version='0.1',
-      description='Light configuration and command line flags',
-      url='https://github.com/altosaar/nomen',
-      author='Jaan Altosaar',
-      author_email='j@jaan.io',
-      license='MIT',
-      packages=['nomen'],
-      install_requires=[
-          'markdown',
-      ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+##########################################################################
+## Imports
+##########################################################################
+
+import os
+import re
+import codecs
+
+from setuptools import setup
+from setuptools import find_packages
+
+##########################################################################
+## Package Information
+##########################################################################
+
+## Basic information
+NAME         = "nomen"
+DESCRIPTION  = "Light command line flags and option / parameter tree"
+AUTHOR       = "Jaan Altosaar"
+EMAIL        = "j@jaan.io"
+LICENSE      = "MIT"
+REPOSITORY   = "https://github.com/altosaar/nomen"
+PACKAGE      = "nomen"
+
+## Define the keywords
+KEYWORDS     = (
+    'nomen', 'python', 'option', 'tree', 'nested', 'dict', 'parameter', 'flags'
+)
+
+## Define the classifiers
+## See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS  = (
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+)
+
+## Important Paths
+PROJECT      = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "DESCRIPTION.txt"
+
+## Directories to ignore in find_packages
+EXCLUDES     = (
+    "tests", "bin", "docs", "fixtures", "register", "notebooks",
+)
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+def read(*parts):
+    """
+    Assume UTF-8 encoding and return the contents of the file located at the
+    absolute path from the REPOSITORY joined with *parts.
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+
+def get_version(path=VERSION_PATH):
+    """
+    Reads the version.py defined in the VERSION_PATH to find the get_version
+    function, and executes it to ensure that it is loaded correctly.
+    """
+    namespace = {}
+    exec(read(path), namespace)
+    return namespace['get_version']()
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    Yields a generator of requirements as defined by the REQUIRE_PATH which
+    should point to a requirements.txt output by `pip freeze`.
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith('#'):
+            yield line
+
+##########################################################################
+## Define the configuration
+##########################################################################
+
+config = {
+    "name": NAME,
+    "version": get_version(),
+    "description": DESCRIPTION,
+    "long_description": read(PKG_DESCRIBE),
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "maintainer": AUTHOR,
+    "maintainer_email": EMAIL,
+    "url": REPOSITORY,
+    "download_url": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "install_requires": list(get_requires()),
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "zip_safe": False,
+}
+
+##########################################################################
+## Run setup script
+##########################################################################
+
+if __name__ == '__main__':
+    setup(**config)
