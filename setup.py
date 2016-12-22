47c28773960d7f75c23954a2be82abadf091d4a4
Jaan Altosaar <jaan.altosaar@gmail.com>
2016-12-22 09:57:26 -0500

    update gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nomen',
+      version='0.1',
+      description='Light configuration and command line flags',
+      url='https://github.com/altosaar/nomen',
+      author='Jaan Altosaar',
+      author_email='j@jaan.io',
+      license='MIT',
+      packages=['nomen'],
+      zip_safe=False)
